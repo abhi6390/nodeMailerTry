aa4fe23c3ec7dbd119ba6ef1555cd301c3f8b7c1
Abhilesh Bhosale <97842957+abhi6390@users.noreply.github.com>
2024-09-30 11:59:01 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { Client, Users } from 'node-appwrite';
+import nodemailer from 'nodemailer';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
+  // Appwrite client setup
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
@@ -12,20 +12,43 @@ export default async ({ req, res, log, error }) => {
 
   try {
     const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
     log(`Total users: ${response.total}`);
   } catch(err) {
     error("Could not list users: " + err.message);
   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
+  // Nodemailer transporter setup
+  const transporter = nodemailer.createTransport({
+    secure: true,
+    host: 'smtp.gmail.com',
+    port: 465,
+    auth: {
+      user: 'newtrial1234567@gmail.com', 
+      pass: 'ldzizfcmbtejsrkm ' 
+    }
+  });
+
+  // Send mail function
+  function sendMail(to, sub, msg) {
+    transporter.sendMail({
+      to: to,
+      subject: sub,
+      html: msg
+    }, (err, info) => {
+      if (err) {
+        error("Error sending email: " + err.message);
+      } else {
+        log("Email sent: " + info.response);
+      }
+    });
   }
 
+  // Check the request path and trigger the email
+  if (req.path === "/ping") {
+    return res.text("Email Sent!");
+  }
+  
+  sendMail('sriharihc733@gmail.com', "Subject", "<p>Hello, this is a test mail.</p>");
   return res.json({
     motto: "Build like a team of hundreds_",
     learn: "https://appwrite.io/docs",
